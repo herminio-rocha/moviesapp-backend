77d014e973adf1ca0b811f959cfe7ac9849fd818
Herminio Rocha <herminiorocha@MacBook-Air-de-Herminio.local>
2025-04-09 09:40:56 -0300

    refactor: remove function get movie detail

--- src/movies/service/movie.service.ts
+++ src/movies/service/movie.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { MovieRepository } from "../repository/movie.repository";
-import { mapNeo4jRecordToMovieDetails } from "../dto/movieDetails.dto";
 import { PaginationInput } from "../dto/paginationInput.dto";
 import { mapNeo4jRecordToMovie } from "../dto/movie.dto";
 import { log } from "console";
@@ -11,7 +10,7 @@ export class MovieService {
 
     constructor(private readonly movieRepository: MovieRepository) { }
 
-    async getMoviesPaginated(pagination: PaginationInput) {
+    async getMovies(pagination: PaginationInput) {
         const records = (await this.movieRepository.findMovies(pagination));
         return records.records.map(mapNeo4jRecordToMovie);
     }
@@ -20,8 +19,4 @@ export class MovieService {
         return (await this.movieRepository.getTotalItems());
     }
 
-    async getMovieDetails(title: string) {
-        const record = await this.movieRepository.findMovieDetails(title);
-        return record.records.map(mapNeo4jRecordToMovieDetails)[0];
-    }
 }
